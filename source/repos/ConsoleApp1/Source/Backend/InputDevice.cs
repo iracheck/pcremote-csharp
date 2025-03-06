cf8ac871bb84bd452d9ec262a93c6f8773ec5a2a
iracheck <irarcheck@gmail.com>
2025-03-05 22:07:43 -0500

    Initial commit

---
+++ source/repos/ConsoleApp1/Source/Backend/InputDevice.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.XInput;
+using WindowsInput;
+using WindowsInput.Native;
+using ControllerToMouse.src.Utils;
+
+namespace ControllerToMouse.src.Backend
+{
+    internal class InputDevice
+    {
+        // External Library Hooks
+        Controller controller;
+        Gamepad status;
+
+        InputSimulator simulator;
+        IMouseSimulator mouse;
+        IKeyboardSimulator keyboard;
+
+
+        // Values
+        private float mouseSpeed;
+
+
+        // Constants
+        private const float MAX_MOUSE_SPEED = 13.0f;
+        
+
+
+        public InputDevice()
+        {
+            controller = new Controller();
+            simulator = new InputSimulator();
+
+            keyboard = simulator.Keyboard;
+            mouse = simulator.Mouse;
+
+        }
+
+        public int pollDevice()
+        {
+            if (controller.IsConnected)
+            {
+                status = controller.GetState().Gamepad;
+                return 1;
+            }
+            return 0;
+        }
+
+        void handleMouseMovement()
+        {
+            int lx = status.LeftThumbX;
+            int ly = status.LeftThumbY;
+
+            if (lx != 0 || ly != 0)
+            {
+                int curX = MouseUtils.GetMousePos().x;
+                int curY = MouseUtils.GetMousePos().y;
+
+                updateMouseSpeed(1);
+
+                mouse.MoveMouseBy(lx, ly);
+            }
+            else
+            {
+                updateMouseSpeed(0);
+            }
+        }
+
+
+        // Mouse Speed uses a state system in order to determine current speed.
+        // 0 -> reset
+        // 1 -> accelerate
+        // -1 -> max speed
+        float updateMouseSpeed(int state)
+        {
+            float accelerationMultiplier = 0.25f; // At what percent of the max speed does the mouse accelerate by per frame
+
+            if (state == 0)
+            {
+                mouseSpeed = 0f;
+            }
+            else if (state == 1)
+            {
+                mouseSpeed = (MAX_MOUSE_SPEED - mouseSpeed) * accelerationMultiplier;
+            }
+            else if (state == -1)
+            {
+                mouseSpeed = MAX_MOUSE_SPEED;
+            }
+            return mouseSpeed;
+        }
+
+        void handleScrolling()
+        {
+            int rx = status.LeftThumbX;
+            int ry = status.LeftThumbY;
+
+            if (ry != 0.0)
+            {
+                mouse.VerticalScroll(ry);
+            }
+            else if (rx != 0.0)
+            {
+                mouse.HorizontalScroll(rx);
+            }
+        }
+
+        //int rx = status.RightThumbX;
+        //int ry = status.RightThumbY;
+    }
+}
---
+++ source/repos/ConsoleApp1/Source/Utils/MouseUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+
+namespace ControllerToMouse.src.Utils
+{
+    internal static class MouseUtils
+    {
+        static private InputSimulator isimulator = new InputSimulator();
+        static private IMouseSimulator mouse = isimulator.Mouse;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ScreenPoint
+        {
+            public int x;
+            public int y;
+        }
+
+
+        // Import GetCursorPos function from user32.dll.
+        // This is a windows library function. Will need to modify this logic for any Linux/Mac versions.
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out ScreenPoint lpPoint);
+
+
+        public static ScreenPoint GetMousePos()
+        {
+            ScreenPoint point;
+            if (GetCursorPos(out point)) {
+                return point;
+            }
+            else
+            {
+                Console.WriteLine("[ERROR > MOUSE] Could not find mouse location. Setting to default (0,0).");
+                point.x = 0;
+                point.y = 0;
+                return point;
+            }
+        }
+
+        public static void resetMousePos()
+        {
+            mouse.MoveMouseTo(0, 0);
+        }
+
+    }
+}

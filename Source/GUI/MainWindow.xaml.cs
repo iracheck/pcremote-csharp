3abf31f6236551aa232f5be8ac532bf8d2313a44
iracheck <137561004+iracheck@users.noreply.github.com>
2025-11-01 18:24:07 -0400

    +Implemented UserControl system and tabmenu for switching between Devices,Profile, and Settings menus

--- Source/GUI/MainWindow.xaml.cs
+++ Source/GUI/MainWindow.xaml.cs
@@ -1,32 +1,93 @@
-﻿using System.Windows;
-using System.Diagnostics;
-
-using ControllerToMouse.Devices;
+﻿using ControllerToMouse.Devices;
+using ControllerToMouse.Source.GUI.Submenus;
 using SharpDX.XInput;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 namespace ControllerToMouse.GUI
 {
     public partial class MainWindow : Window
     {
+        // Menus
+        private readonly UserControl DeviceMenu = new DevicesMenu();
+        private readonly UserControl SettingMenu = new SettingsMenu();
+        private readonly UserControl ProfileMenu = new ProfilesMenu();
+        private List<Button> NavButtons;
+
         public MainWindow()
         {
+            Trace.WriteLine("Init main window");
+
             InitializeComponent();
+
+            NavButtons = new List<Button>
+            {
+                DevicesTab,
+                ProfilesTab,
+                SettingsTab
+            };
+
+            ContentArea.Content = DeviceMenu;
+
         }
 
-        protected void OnExit(object sender, EventArgs e)
+        protected void MenuButtonClick(object sender, EventArgs e)
         {
-            InputDeviceManager.RemoveAllDevices();
-            base.OnClosed(e);
+            try { 
+                SetActiveTab(sender as Button);
+            } 
+            catch { Debug.WriteLine("Tried to trigger a menu button, but the triggered object wasnt a button! "); }
+        }
+
+        protected void SetActiveTab(Button newTab)
+        {
+            Trace.WriteLine("Trying to switch tab");
+            foreach (var b in NavButtons)
+            {
+                b.Tag = null;
+            }
+
+            newTab.Tag = "Active";
+
+            switch (newTab.Name)
+            {
+                case ("DevicesTab"):
+                    ContentArea.Content = DeviceMenu;
+                    break;
+                case ("ProfilesTab"):
+                    ContentArea.Content = ProfileMenu;
+                    break;
+                case ("SettingsTab"):
+                    ContentArea.Content = SettingMenu;
+                    break;
+            }
+        }
+
+
+        // Header
+        protected void HeaderMouseButton_Down(object sender, EventArgs e)
+        {
+            DragMove();
         }
 
         protected void MinimizeButton_Click(object sender, EventArgs e)
         {
-
+            WindowState = WindowState.Minimized;
         }
 
-        protected void CloseButton_Click(object sender, EventArgs e)
+        protected void ExitButton_Click(object sender, EventArgs e)
         {
+            Application.Current.Shutdown();
+        }
 
+
+        // Generic Listeners
+        protected void OnExit(object sender, EventArgs e)
+        {
+            InputDeviceManager.RemoveAllDevices();
+            base.OnClosed(e);
         }
 
     }
